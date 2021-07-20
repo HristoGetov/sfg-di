f96fecaf2c3468285b03bcb3bdc278cf1822a35f
HristoGetov <260064@via.dk>
2021-07-20 23:39:47 +0300

    New lectures from the course added.

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -2,16 +2,24 @@ package guru.springframework.sfgdi;
 
 
 import guru.springframework.sfgdi.controllers.*;
+import guru.springframework.sfgdi.services.PrototypeBean;
+import guru.springframework.sfgdi.services.SingletonBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+//@ComponentScan(basePackages = {"guru.springframework.sfgdi","com.springframework.pets"})
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
 
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		PetController petController = ctx.getBean("petController", PetController.class);
+		System.out.println("The Best Pet is: ");
+		System.out.println(petController.whichPetIsTheBest());
 
 		I18nController i18nController =(I18nController) ctx.getBean("i18nController");
 		System.out.println(i18nController.sayGreeting());
@@ -32,6 +40,20 @@ public class SfgDiApplication {
 		System.out.println("----- Constructor");
 		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
 		System.out.println(constructorInjectedController.getGreeting());
+
+		System.out.println("-------- Bean Scopes ---------");
+		SingletonBean singletonBean1 = ctx.getBean(SingletonBean.class);
+		System.out.println(singletonBean1.getMyScope());
+		SingletonBean singletonBean2 = ctx.getBean(SingletonBean.class);
+		System.out.println(singletonBean2.getMyScope());
+
+
+
+		PrototypeBean prototypeBean1 = ctx.getBean(PrototypeBean.class);
+		System.out.println(prototypeBean1.getMyScope());
+		PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
+		System.out.println(prototypeBean2.getMyScope());
+
 
 	}
 
